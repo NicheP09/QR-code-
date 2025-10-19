77ea95993cf320017807224509bd85725b83bcb6
peter <peteryeboah09@gmail.com>
2025-10-19 16:42:49 +0100

    intial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="bg-slate-300 h-screen flex justify-center items-center">
+      hey
+      <h1 className="">Hello </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
